d0c9308185b408c5ae25c7ff5be7d4e5c840e029
totalerex <42652300+totalerex@users.noreply.github.com>
2018-11-09 10:38:15 +0200

    type mismatch dirty fix
    after some update type mismatch  error arrived

--- device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/models/Calendar.kt
+++ device_calendar/android/src/main/kotlin/com/builttoroam/devicecalendar/models/Calendar.kt
@@ -1,6 +1,6 @@
 package com.builttoroam.devicecalendar.models
 
-class Calendar(val name: String, val color: Long) {
+class Calendar(val name: String?, val color: Long?) {
     var id: String? = null
     var isReadOnly: Boolean = false
     var accountName: String? = null
